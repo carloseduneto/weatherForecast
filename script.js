8953f9fd9fc623083986fdf57dea3f94eb6a37ae
Carlos Eduardo Neto <112590775+carloseduneto@users.noreply.github.com>
2024-05-27 17:25:54 -0300

    fix: :bug:

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const apiKey = 'd7db451187ac48b4bd7130555242305';
-const cityName = 'Guardinha';
-const apiUrlCurrent = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&lang=pt`;
-const apiUrlWeatherForecast = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=3&lang=pt`;
-
-const cacheWeatherForecastKey = 'forecastData'
-const cacheKey = 'weatherData';
+var cityName = 266828;
+let cacheWeatherForecastKey = 'forecastData'
+let cacheKey = 'weatherData';
 // const cacheExpiry = 3600000; // 1 hour in milliseconds
 const cacheExpiry = 3600000; // 1 hour in milliseconds
 
@@ -15,6 +12,7 @@ let wet = document.getElementById("wet")
 let precip_in = document.getElementById("precip_in")
 let last_updated = document.getElementById("last_updated")
 let threeDays = document.getElementsByClassName("threeDays")
+let body = document.getElementById("body")
 
 let cloudy = [
   "Parcialmente nublado",
@@ -83,7 +81,8 @@ let storm=[
 
 console.log(threeDays[0])
 
-async function fetchWeatherData() {
+
+/*async function fetchWeatherData() {
       fetch(apiUrlCurrent)
         .then(response => response.json())
         .then(data => {
@@ -94,48 +93,97 @@ async function fetchWeatherData() {
       })
       .catch(error => console.error('Error:', error));
     }
+*/
 
-
-// console.log("A")
-
-
-async function getWeatherData() {
-  const cachedData = localStorage.getItem(cacheKey);
-  if (cachedData) {
-    const { data, timestamp } = JSON.parse(cachedData);
-    if (Date.now() - timestamp < cacheExpiry) {
+async function fetchWeatherData(cityName) {
+  let cityName2 = cityName
+  let apiUrlCurrent = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=id:${cityName2}&lang=pt`;
+  try{
+    const response = await fetch(apiUrlCurrent);
+    const data = await response.json();
+    localStorage.setItem(cacheKey, JSON.stringify({ data, timestamp: Date.now()}));
+    console.log(data)
+    
+    
+    // console.log("A")
+    
+    
+    
+    // let cachedData = localStorage.getItem(cacheKey);
+    let status1;
+    // if (cachedData) {
+    // const { data, timestamp } = JSON.parse(cachedData);
+    /*if (Date.now() - timestamp < cacheExpiry) {
+      console.log('✅ Using cached data');*/
       console.log(data)
-      console.log('✅ Using cached data');
       console.log(data.location.name);
       console.log(data.location.region);
       console.log(data.location.country);
-
+      
       console.log(data.current.condition.text)
       console.log(data.current.feelslike_c +" ºC")
-
+      
       city.innerHTML = data.location.name
-
+      
       temperature.innerHTML = data.current.temp_c +"ºC"
-
-      status.innerHTML = data.current.condition.text
-
+      
+      status1 = data.current.condition.text
+      status.innerHTML = status1
+      
       wet.innerHTML = data.current.humidity+"%"
-
-
-
+      
+      
+      
       last_updated.innerHTML = data.current.last_updated.split(' ')[1]
-
-
+      
+      if(rainy.includes(status1)){
+        body.style.backgroundImage = "url(https://images.unsplash.com/photo-1603321544554-f416a9a11fcf?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+      }
+      
+      
+      if(cloudy.includes(status1)){
+        body.style.backgroundImage = "url(https://images.unsplash.com/photo-1445264618000-f1e069c5920f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+      }
+      
+      if(blizzard.includes(status1)){
+        body.style.backgroundImage = "url(https://images.unsplash.com/photo-1547576962-9f4ee7e7a7c1?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+      }
+      
+      if(rainySnowy.includes(status1)){
+        body.style.backgroundImage = "url(https://images.unsplash.com/photo-1542601098-8fc114e148e2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+      }
+      
+      if(snowy.includes(status1)){
+        body.style.backgroundImage = "url(https://images.unsplash.com/photo-1545858908-bc6fee2bd44d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+      }
+      
+      if(storm.includes(status1)){
+        body.style.backgroundImage = "url(https://images.unsplash.com/photo-1537036017783-64573b29adb9?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+      }
+      
+      if(status1.includes("Sol")){
+        body.style.backgroundImage = "url(https://images.unsplash.com/photo-1601297183305-6df142704ea2?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+      }
+      
+      if(status1.includes("Céu limpo")){
+        body.style.backgroundImage = "url(https://images.unsplash.com/photo-1581886573745-4487c55d95f8?q=80&w=1554&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
+      }
+      
+      
       return;
-    }
+    }catch (error){
+    console.error("Error: ", error)
   }
+}
+  // }
+/*
   console.log('🔄 Fetching new data');
   fetchWeatherData();
-}
+}*/
 
 
 
-
+/*
 async function fetchWeatherForecast() {
   fetch(apiUrlWeatherForecast)
     .then(response => response.json())
@@ -150,32 +198,42 @@ async function fetchWeatherForecast() {
 
 
 }
+*/
 
-
-async function getForecastData() {
-  let nextDays="";
-  let nextCondition;
-  let iconCondition;
-
-  const cachedData = localStorage.getItem(cacheWeatherForecastKey);
-  if (cachedData) {
-    const { data, timestamp } = JSON.parse(cachedData);
-    if (Date.now() - timestamp < cacheExpiry) {
-      console.log('✅ Using cached data');
+async function fetchWeatherForecast(cityName) {
+  let cityName2 = cityName
+  let apiUrlWeatherForecast = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=id:${cityName2}&days=3&lang=pt`;
+  try {
+    const response = await fetch(apiUrlWeatherForecast);
+    const data = await response.json();
+    // localStorage.setItem(cacheWeatherForecastKey, JSON.stringify({ data, timestamp: Date.now() }));
+    // displayWeatherData(data);
+    console.log(data);
+    
+    
+    let nextDays="";
+    let nextCondition;
+    let iconCondition;
+    
+    // const cachedData = localStorage.getItem(cacheWeatherForecastKey);
+    // if (cachedData) {
+    // const { data, timestamp } = JSON.parse(cachedData);
+    /*if (Date.now() - timestamp < cacheExpiry) {
+      console.log('✅ Using cached data');*/
       for (let index = 0; index < data.forecast.forecastday.length; index++) {
         console.log(brazilianDate(data.forecast.forecastday[index].date))
         console.log(data.forecast.forecastday[index].day.condition.text)
         console.log(data.forecast.forecastday[index].day.avgtemp_c + "ºC")
-
+        
         let dayCondition = (data.forecast.forecastday[index].day.condition.text)
-
+        
         if(rainy.includes(dayCondition)){
           nextCondition = "Chuva"
           iconCondition = `<span class="material-symbols-outlined">
           rainy
           </span>`
         }
-
+        
         if(storm.includes(dayCondition)){
           nextCondition = "Tempestade"
           iconCondition = `<span class="material-symbols-outlined">
@@ -189,43 +247,43 @@ async function getForecastData() {
           cloudy_snowing
           </span>`
         }
-
+        
         if(rainySnowy.includes(dayCondition)){
           nextCondition = "Chuva com neve"
           iconCondition = `<span class="material-symbols-outlined">
           rainy_snow
           </span>`
         }
-
+        
         if(cloudy.includes(dayCondition)){
           nextCondition = "Nublado"
           iconCondition = `<span class="material-symbols-outlined">
           cloud
           </span>`
         }
-
+        
         if(blizzard.includes(dayCondition)){
           nextCondition = "Nevasca"
           iconCondition = `<span class="material-symbols-outlined">
           snowing_heavy
           </span>`
         }
-      
-
+        
+        
         if(dayCondition.includes("Sol")){
           nextCondition = "Sol"
           iconCondition = `<span class="material-symbols-outlined">
           sunny
           </span>`
         }
-
+        
         if(dayCondition.includes("Céu limpo")){
           nextCondition = "Céu limpo"
           iconCondition =  `<span class="material-symbols-outlined">
           clear_night
           </span>`
         }
-
+        
         nextDays+= 
         `<div class="nextsDays">
         <span>${brazilianDate(data.forecast.forecastday[index].date)}</span>
@@ -241,19 +299,22 @@ async function getForecastData() {
       }
       threeDays[0].innerHTML= nextDays
       precip_in.innerHTML = data.forecast.forecastday[0].day.daily_chance_of_rain
-+"%";
-
+      +"%";
+      
       console.log(data)
-
+      
       return;
-    }
+    }catch (error) {
+    console.error('Error:', error);
   }
-  console.log('🔄 Fetching new data');
-  fetchWeatherForecast();
 }
 
-getWeatherData();
-getForecastData();
+  /*console.log('🔄 Fetching new data');
+  fetchWeatherForecast();
+}*/
+
+fetchWeatherData(cityName);
+fetchWeatherForecast(cityName);
 
 
 function brazilianDate(date) {
@@ -265,7 +326,7 @@ function brazilianDate(date) {
   const day = date.slice(-2)
   
   const month = date.slice(5,7) 
-
+  
   const year = date.slice(0,4)
   
   // Retorna a data formatada no padrão brasileiro
@@ -273,11 +334,11 @@ function brazilianDate(date) {
 }
 
 
+let results = document.getElementById("results")
 function toSearch() {
-  let search = document.getElementById("search").value
-  let results = document.getElementById("results")
-  const apiUrlCity = `http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${search}&lang=pt`;
   let responses="";
+  let search = document.getElementById("search").value
+  const apiUrlCity = `http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${search}&lang=pt`;
   
   if (search !== null && search !== undefined && search !== '') {
     fetch(apiUrlCity)
@@ -287,7 +348,13 @@ function toSearch() {
         console.log(data[0])   
         
         for (let index = 0; index < data.length; index++) {
-          responses += `<span class="results" onclick='changeCity()'>${data[index].name}, ${data[index].name}</span>`
+          console.log(data)
+          if(data[index].region!=""){
+            responses += `<span class="results" onclick='changeCity(${data[index].id})'>${data[index].name}, ${data[index].region}</span>`
+          }else{
+            responses += `<span class="results" onclick='changeCity(${data[index].id})'>${data[index].name}, ${data[index].country}</span>`
+          }
+
           console.log(data[index])
         }
         // results.innerHTML = data
@@ -297,7 +364,6 @@ function toSearch() {
       .catch(error => console.error('Error:', error));
       
       
-      console.log(data)
       
     } else {
       results.innerHTML = "<span></span>"
@@ -307,8 +373,14 @@ function toSearch() {
   }
   
 
-function changeCity(params) {
-  
+function changeCity(cityId) {
+  let search = document.getElementById("search").value = ""
+  results.innerHTML = ""
+  console.log("Id Cidade",cityId)
+  localStorage.removeItem(cacheKey)
+  localStorage.removeItem(cacheWeatherForecastKey)
+  fetchWeatherData(cityId)
+  fetchWeatherForecast(cityId)
 }
 
 /*
@@ -441,5 +513,3 @@ geCityData();
 // }
 
 // console.log(removeQuotationMarks("abc"))
-
-// console.log(forecast())
